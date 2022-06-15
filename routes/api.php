72e98f2733aa6c997b7b3dd51b8e66ab304a5b15
lucas <luqui31@gmail.com>
2022-06-15 16:37:28 +0200

    MAJ: categories controller

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
 Route::prefix('posts')->name('posts.')->group(function () {
     Route::get('/', [PostsController::class, 'index']);
     Route::get('/view/{id}', [PostsController::class, 'view']);
