22ad318a701a4a20c429c9e8ba0e80be6348ba80
JKxKookie <mariaguydefontgalland@gmail.com>
2022-06-15 23:13:50 +0200

    API request to fetch all the posts with AXIOS

--- resources/js/components/ListAnnonces.js
+++ resources/js/components/ListAnnonces.js
@@ -1,38 +1,122 @@
 import React from "react";
+import CategoryIcon from '@mui/icons-material/Category';
 import css from "../../../public/css/listAnnonces.css";
-
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { useState, useEffect } from 'react';
+import FlatList from 'flatlist-react';
+import {Link} from "react-router-dom";
 
 const ListAnnonces = () => {
+    
+    //let loading = true; 
+    // Fetch data from database to get list of posts and category
+    const [state, setData] = useState({
+        posts: '', 
+        listCategories:''
+    });
+    const fetchPosts = async () => {
+        const apiPosts = await axios.get("http://127.0.0.1:8000/api/posts");
+        const apiCategories = await axios.get("http://127.0.0.1:8000/api/posts/add");
+
+        console.log(apiPosts);
+
+        setData({
+            posts: await apiPosts.data.data, 
+            listCategories: await apiCategories.data.data
+        });
+
+        // find category name for each post
+        for (let post of apiPosts.data.data ) {
+            console.log(post);
+            //console.log(post.category_id);
+            for (let category of apiCategories.data.data) {
+                if (category.id === post.category_id ) {
+                    post.category_name = category.title;
+                    console.log(post.category_name);
+                }
+            }
+        }
+    };
+    useEffect(() => {
+            fetchPosts();
+
+       }, []);
+    //console.log("----------");
+    console.log(state.posts);
+    console.log(state.listCategories);
+  
 
     return (
         <section id="annonces">
 
             <div  id="filtres">
                 <h2>Filtres</h2>
-                <p>
+                <p id="filtres__description">
                     Aidez-vous des filtres afin de repérer les annonces
                     qui peuvent vous intéresser.
                 </p>
 
-                <div className="filtres_container">
-                    <label>Catégorie</label>
-                    <input type="text" id="category" placeholder="Exemple : Bricolage" name="category"/>
-                </div>
 
-                <div className="filtres_container">
-                    <label>Date</label>
-                    <input type="text" id="date" placeholder="24/06/2022" name="date"/>
-                </div>
-                <div className="filtres_container">
-                    <label>Durée du service (en heures)</label>
-                    <input type="number" id="lengthService" placeholder="1 heure" name="lengthService" minLength="4" maxLength="8"/>
-                </div>
+                {/* form to filter the posts */}
+                <form action="#" method="post">
+                    <div className="filtres_container">
+                        <label htmlFor="category">Catégorie</label>
+                        <select id="category" name="list" >
+                            <FlatList list={state.listCategories} renderItem={item =>
+                            <option value="category">{item.title}</option> }/>
+                        </select>
+                    </div>
 
-                <button className="button-blue">Filtrez les annonces</button>
+                    <div className="filtres_container">
+                        <label htmlFor="date">Date</label>
+                        <input type="text" id="date" placeholder="24/06/2022" name="date"/>
+                    </div>
+                    <div className="filtres_container">
+                        <label htmlFor="lengthService">Durée du service (en heures)</label>
+                        <select id="lengthService" name="lengthService" >
+                            <option value="15">15 mins</option> 
+                            <option value="30">30 mins</option> 
+                            <option value="45">45 mins</option> 
+                            <option value="90">1h30 mins</option> 
+                            <option value="120">2h</option> 
+                            <option value="150">+ de 2h</option> 
+                        </select>
+                    </div>
+
+                    <button className="blue-btn">Filtrez les annonces</button>
+                </form>
             </div>
 
             <div id="listAnnonces">
                 <h2>Liste des annonces</h2>
+                <div id="listAnnonces__container">
+
+                <FlatList list={state.posts} renderItem={item => 
+                    <div className="annonce">
+                        <img src="#" alt=""/>
+                        <div className="annonce__infos">
+
+                            <h3>{item.title}</h3>
+                            <div className="annonce__infos">
+                                <div className="annonce__infosCategorie">
+                                    <CategoryIcon style={{ color: '#5BB286', fontSize:30}}/>
+                                    <p>{item.category_name}</p>
+                                </div>
+                                <div className="annonce__infosDate">
+                                    <CalendarMonthIcon style={{ color: '#5BB286', fontSize:30}}/>
+                                    <p>19/06/2022</p>
+                                </div>
+                            </div>
+                            
+                            <div>
+                                <button className="blue-btn"><Link to={`/annonce?id=${item.id}`}className="annonce__link">Voir l'annonce</Link></button>
+                                <button className="blue-btn">Rendre service</button>
+                            </div>
+                        </div>
+                    </div>            
+            }
+                />                    
+                </div>
             </div>
         </section>
     );
