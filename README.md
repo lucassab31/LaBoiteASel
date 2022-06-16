59e2d20976570e7845650b70eceb76bde0ae9503
marion dieudonné <m.j.dieudonne@orange.fr>
2022-06-16 11:51:23 +0200

    version mobile messagerie + mixins

--- README.md
+++ README.md
@@ -11,3 +11,11 @@
 - **post | toolsProvided :** *Y* -> Oui, *N* -> Non; *A* -> aucun outils nécéssaire
 - **post | status :** *C* -> crée, *P* -> en cours de résolution; *F* -> clôturé
 - **reports | status :** *C* -> crée, *P* -> en cours de résolution; *F* -> clôturé
+
+## CSS
+- ne pas commit les fichiers contenus dans public/css et public/js : ils sont compilés automatiquement lorsque la commande npm run watch est lancée (il faudra juste penser à les commit
+à la fin du projet).
+- ajouter les noms des fichiers scss (sans l'extension) dans l'array du fichier webpack.mix.js pour
+la compilation puis importer le fichier css (contenu dans public/css) dans le component correspondant.
+- Penser à utiliser les variables dans le fichier _variables, et à créer des mixins pour éviter de dupliquer la scss si possible
+- npm run watch pour compiler en temps réeel - npm run dev pour compiler une seule fois
---
+++ resources/js/components/Messaging.js
@@ -0,0 +1,52 @@
+import React from "react";
+import css from "../../../public/css/messaging.css"
+
+const Messaging = () => {
+
+    return (
+        <main>
+            <h1> Ma messagerie </h1>
+            <div id="bloc" className="bloc bloc-messaging bloc--1-3">
+                <div className="bloc-chat">
+                    <div className="bloc-profil"> 
+                        <img className="bloc-profil__img" src="../../images/exemple-profile.png"></img>
+                        <div className="bloc-profil__infos">
+                            <p className="bloc-infos__name"> Nathalie Sims </p>
+                            <p className="bloc-infos__last"> Mon dernier message </p>
+                            <p className="bloc-infos__date"> 25/06/2034 10:34 </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="bloc-messages"> 
+                    <div className="bloc-messages__wrapper">
+                        <div className="bloc-message bloc-message--send">
+                            <p className="bloc-message__text"> 
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.  
+                            </p>
+                            <p className="bloc-message__date"> 25/06/2034 10:34 </p>
+                        </div>
+                        <div className="bloc-message bloc-message--received">
+                            <p className="bloc-message__text"> 
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.  
+                            </p>
+                            <p className="bloc-message__date"> 25/06/2034 10:34 </p>
+                        </div>
+                        <div className="bloc-message bloc-message--send">
+                            <p className="bloc-message__text"> 
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.  
+                            </p>
+                            <p className="bloc-message__date"> 25/06/2034 10:34 </p>
+                        </div>
+                    </div>
+                    <form className="bloc-messages__content bloc--bg-blue">
+                    <label for="message"> Entrez votre message </label>
+                    <textarea placeholder="Exemple : Bonjour..." id="message"/>
+                    <button class="button button-white" type="submit"> Envoyer le message </button>
+                    </form>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Messaging;
