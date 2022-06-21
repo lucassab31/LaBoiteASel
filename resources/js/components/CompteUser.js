e62e06165369a751a28c505989020b7dbd684c80
Emilie Moinet <emilie.moinet60@gmail.com>
2022-06-21 14:30:33 +0200

    compte user css

--- resources/js/components/CompteUser.js
+++ resources/js/components/CompteUser.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import EditIcon from '@mui/icons-material/Edit';
+import { Link } from "react-router-dom";
 import ReportProblemIcon from '@mui/icons-material/ReportProblem';
 import "../../../public/css/compteUser.css"
 import { useEffect } from "react";
@@ -26,23 +27,24 @@ const CompteUser = () => {
             <div className="bloc-white">
                 <img className="bg-image" alt="" src="../../../images/jardin-profile.jpg"></img>
                 <div className="user-image"><img alt="" src="../../../images/exemple-profile.png"></img><p>{resp.firstName} {resp.lastName}</p></div>
+                <div className="bloc-menu-line"></div>
                 <div className="bloc-menu">
-                    <p className="menu-option">À propos</p>
-                    <p>Mes annonces</p>
+                    <Link className="menu-option" to="">Mes annonces</Link>
+                    <Link className="menu-option" to="">À propos</Link>
                     <Button className="menu-button">
                         <EditIcon aria-hidden="true" style={{ color: '#5BB286', fontSize: 15 }} />Modifier le profil
                     </Button>
-                    <Button className="menu-button">
+                    {/* <Button className="menu-button">
                         <ReportProblemIcon aria-hidden="true" style={{ color: '#5BB286', fontSize: 15 }} />Signaler le profil
-                    </Button>
+                    </Button> */}
                 </div>
             </div>
             <div className="bloc-red">
                 <div className="left-panel">
                     <h3 className="panel-title">Informations</h3>
-                    <p>Téléphone : { resp.phone }</p>
-                    <p>Adresse : { resp.address }</p>
-                    <p>Ville : { resp.city }</p>
+                    <p>Téléphone : {resp.phone}</p>
+                    <p>Adresse : {resp.address}</p>
+                    <p>Ville : {resp.city}</p>
                 </div>
                 <div className="right-panel">
                     <h3 className="panel-title">Statistiques des services rendus</h3>
