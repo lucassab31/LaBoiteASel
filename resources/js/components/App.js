0862b2e95ece40743edd9c135fe7f5b6720275cd
marion dieudonné <m.j.dieudonne@orange.fr>
2022-06-21 15:29:53 +0200

    début de structure panel admin

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -14,6 +14,7 @@ import CompteUser from './CompteUser';
 import Faq from "./FAQ";
 import FormAnnonce from './FormAnnonce';
 import {Helmet} from "react-helmet";
+import PanelAdmin from "./PanelAdmin";
 
 function App() {
     return (
@@ -35,6 +36,7 @@ function App() {
                     <Route path="/messaging/*" element={<Messaging/>}/>
                     <Route path="/tutoriel" element={<Faq/>}/>
                     <Route path="/create" element={<FormAnnonce/>}/>
+                    <Route path="/admin/*" element={<PanelAdmin/>}/>
                 </Routes>
 
                 <Footer/>
