51a9fd821c8c0b46d81b15dd71ce53dddacee0a6
JKxKookie <mariaguydefontgalland@gmail.com>
2022-06-21 16:35:01 +0200

    added autocomplete attributes to the form

--- resources/js/components/FormNewUser.js
+++ resources/js/components/FormNewUser.js
@@ -22,6 +22,7 @@ const FormNewUser = () => {
                     <input 
                         aria-required="true" 
                         data-name="firstname" 
+                        autoComplete="given-name"
                         id="firstname" 
                         type="text" 
                         placeholder="Prénom"/>
@@ -32,6 +33,7 @@ const FormNewUser = () => {
                     <input 
                         aria-required="true" 
                         data-name="lastname" 
+                        autoComplete="family-name"
                         id="lastname" 
                         type="text" 
                         placeholder="Nom"/>
@@ -43,23 +45,33 @@ const FormNewUser = () => {
                         aria-required="true" 
                         data-name="email" 
                         id="email" 
+                        autoComplete="email"
                         type="mail" 
                         placeholder="Adresse email"/>
                 </div>
 
                 <div>
                     <label htmlFor="phone">Numéro de téléphone</label>
-                    <input aria-required="true" data-name="phone"  type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder="Numéro de téléphone"/>
+                    <input 
+                    aria-required="true" autoComplete="tel-national"
+                    data-name="phone"  type="tel" id="phone" name="phone" 
+                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder="Numéro de téléphone"/>
                 </div>
 
                 <div>
                     <label htmlFor="password">Mot de passe</label>
-                    <input aria-required="true" data-name="password"  type="password" id="password" name="password" minLength="8" required placeholder="Un mot de passe temporaire"/>
+                    <input 
+                        aria-required="true" autoComplete="new-password"
+                        data-name="password" 
+                        type="password" 
+                        id="password" name="password" minLength="8" 
+                        required placeholder="Un mot de passe temporaire"/>
                 </div>
 
                 <div>
                     <label htmlFor="dateBirth">Date de naissance</label>
                     <input  
+                        autoComplete="bday"
                         data-name="dateBirth" 
                         id="dateBirth" 
                         type="datetime-local"
@@ -72,6 +84,7 @@ const FormNewUser = () => {
                         <input 
                             aria-required="true" 
                             data-name="adress" 
+                            autoComplete="street-address" 
                             id="adress" 
                             type="text" 
                             placeholder="Adresse"/>
@@ -101,6 +114,8 @@ const FormNewUser = () => {
                         />
                     </div>
                 </div>
+
+                <button className="button-yellow"> Ajouter un nouveau membre </button>
             </form>
         </main>
     );
