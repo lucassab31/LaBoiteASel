631c27c0ae896fb2b164f7dd88cdb01c221e818c
JKxKookie <mariaguydefontgalland@gmail.com>
2022-06-22 14:45:25 +0200

    display list of users done (API request and flatlist)

--- resources/js/components/UsersList.js
+++ resources/js/components/UsersList.js
@@ -5,6 +5,7 @@ const API_URL = process.env.MIX_APP_URL +'api/';
 
 const UsersList = () => {
     require ("../../../public/css/panelAdmin.css");
+    let  baseUrl = API_URL+"admin/users/"; 
 
     const [state, setData] = useState({
         listUsers:'',
@@ -13,11 +14,11 @@ const UsersList = () => {
     // API request to fetch list of users 
     const fetchUsers = async () => {
         const apiUsers = await axios.get(baseUrl);
-
+        
         setData({
             listUsers: await apiUsers.data.data, 
         });
-        //console.log(apiPosts.data.data);
+        console.log(apiUsers.data.data);
     };
     
     useEffect(() => {
@@ -27,14 +28,29 @@ const UsersList = () => {
     return(
         <main>
                 <h2> Liste des membres </h2>
-                <div className="block--1-5">
-<<<<<<< HEAD
-=======
 
-                                
->>>>>>> list users component created
+                <div className="listUsers">
+                <table>
+                        <caption> Affichage des informations des membres</caption>
+                        <thead>
+                            <tr>
+                                <th scope="col">Nom & Prénom</th>
+                                <th scope="col">Adresse mail</th>
+                                <th scope="col">Grains de sels restants</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <FlatList keyExtractor={(item) => item.id} list={state.listUsers}  renderItem={item => 
+                            <tr>
+                                <td>{item.firstName} {item.lastName}</td>
+                                <td>{item.email}</td>
+                                <td>{item.money}</td>
+                            </tr>          
+                            }
+                        />  
+                        </tbody>
+                    </table>       
                 </div>
-    
         </main>
     )
 }
