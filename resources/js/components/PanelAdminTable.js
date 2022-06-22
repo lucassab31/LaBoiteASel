2f4b3bed4111f51d10ed78d6938a0241ca18b9ba
marion dieudonné <m.j.dieudonne@orange.fr>
2022-06-22 11:34:29 +0200

    navigation + UsersList file

---
+++ resources/js/components/PanelAdminTable.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NavigationPanelAdmin from './NavigationPanelAdmin';
+import PanelAdminHome from './PanelAdminHome';
+
+
+const PanelAdminTable = () => {
+    require ("../../../public/css/panelAdmin.css");
+
+    return(
+        <main>
+                <h2> Hello </h2>      
+        </main>
+    )
+}
+
+export default PanelAdminTable;
