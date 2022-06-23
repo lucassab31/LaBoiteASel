58293a5f366c474dbb969218c317b654c2ab16e7
lucas <luqui31@gmail.com>
2022-06-23 11:32:46 +0200

    MAJ: api routing final

--- routes/api.php
+++ routes/api.php
@@ -23,19 +23,23 @@ Route::prefix('posts')->name('posts.')->group(function () {
     Route::get('/', [PostsController::class, 'index']);
     Route::get('/view/{id}', [PostsController::class, 'view']);
     Route::get('/add', [PostsController::class, 'add']);
-   
+    Route::get('/postsFiltered/{category}/{lengthservice}/{date}/{dateType}', [PostsController::class, 'filteredPosts']);
+
+    // Posts connected
     Route::middleware('auth:api')->group(function () {
         Route::post('/add', [PostsController::class, 'store']);
         Route::get('/candidate/{id}', [PostsController::class, 'candidate']);
         Route::get('/progress/{id}', [PostsController::class, 'inProgress']);
         Route::get('/finish/{id}', [PostsController::class, 'finish']);
+        
         Route::get('/delete/{id}', [PostsController::class, 'delete']);
     });
-    Route::get('/postsFiltered/{category}/{lengthservice}/{date}/{dateType}', [PostsController::class, 'filteredPosts']);
 });
 
+// Connection
 Route::post('/users/login', [UsersController::class, 'login']);
-
+// User profile
+Route::prefix('users')->get('/view/{id}', [UsersController::class, 'view']);
 
 // Routes connected
 Route::middleware('auth:api')->group(function () {
@@ -43,7 +47,6 @@ Route::middleware('auth:api')->group(function () {
     Route::prefix('users')->name('users.')->group(function () {
         Route::get('/logout', [UsersController::class, 'logout']);
 
-        Route::get('/view/{id}', [UsersController::class, 'view']);
         Route::get('/viewPosts', [UsersController::class, 'viewPosts']);
         Route::get('/viewTransactions', [UsersController::class, 'viewTransactions']);
         Route::get('/viewReports', [UsersController::class, 'viewReports']);
@@ -55,26 +58,24 @@ Route::middleware('auth:api')->group(function () {
     
     // Routes admin
     Route::middleware('isAdmin')->prefix('admin')->group(function () {
+        // Users
+        Route::prefix('users')->name('users.')->group(function () {
+            Route::get('/', [AdminUsersController::class, 'index']);
+            Route::get('/view/{id}', [AdminUsersController::class, 'view']);
+        
+            Route::get('/add', [AdminUsersController::class, 'add']);
+            Route::post('/add', [AdminUsersController::class, 'store']);
 
-    });
-});
-// Routes admin
-Route::prefix('admin')->group(function () {
-    // Users
-    Route::prefix('users')->name('users.')->group(function () {
-        Route::get('/', [AdminUsersController::class, 'index']);
-        Route::get('/view/{id}', [AdminUsersController::class, 'view']);
-    
-        Route::get('/add', [AdminUsersController::class, 'add']);
-        Route::post('/add', [AdminUsersController::class, 'store']);
+            Route::post('changeUserStatus/{id}', [AdminUsersController::class, 'changeUserStatus']);
 
-        Route::get('/delete/{id}', [AdminUsersController::class, 'delete']);
-    });
+            Route::get('/delete/{id}', [AdminUsersController::class, 'delete']);
+        });
 
-    // Stats
-    Route::prefix('stats')->name('stats.')->group(function () {
-        Route::get('/', [AdminStatsController::class, 'index']);
-        Route::post('/balance', [AdminStatsController::class, 'balance']);
-        Route::get('/export', [AdminStatsController::class, 'exportCsv']);
+        // Stats
+        Route::prefix('stats')->name('stats.')->group(function () {
+            Route::get('/', [AdminStatsController::class, 'index']);
+            Route::post('/balance', [AdminStatsController::class, 'balance']);
+            Route::get('/export', [AdminStatsController::class, 'exportCsv']);
+        });
     });
 });
