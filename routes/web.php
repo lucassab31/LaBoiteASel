3c8f185a8fcf84442d6240d626133336a71b90cb
lucas <luqui31@gmail.com>
2022-06-23 14:54:45 +0200

    MAJ: route user

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{path?}', function () {
     return view('welcome');
 })->name('home');
+Route::get('/utilisateur/{path?}', function () {
+    return view('welcome');
+})->name('user');
 Route::get('/admin/{path?}', function () {
     return view('welcome');
 })->name('admin');
